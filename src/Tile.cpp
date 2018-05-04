f9812cffb18ca66f66973197f8f57614861010b2
danwardvs <danwardvs@adsgames.net>
2018-05-04 19:06:23 +0000

    Tile maps stufffffffffffff

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,12 +1,24 @@
 #include "Tile.h"
 
-Tile::Tile(int newX, int newY)
+Tile::Tile(int newX, int newY, int newType)
 {
-    sprite = tools::load_bitmap_ex("images/tiles/Grass.png");
+    type=newType;
+
+    if(type==0)
+      sprite = tools::load_bitmap_ex("images/tiles/Grass.png");
+     if(type==1)
+      sprite = tools::load_bitmap_ex("images/tiles/Path.png");
+
+
     grid = tools::load_bitmap_ex("images/tiles/EmptyGrid.png");
 
     x = newX;
     y = newY;
+
+  int bigx=x*64;
+  int bigy=y*64;
+  iso_x = bigx - bigy;
+  iso_y = (bigx + bigy) / 2;
 
 }
 
@@ -16,12 +28,9 @@ Tile::~Tile()
 }
 
 void Tile::draw(){
-  int bigx=x*64;
-  int bigy=y*64;
-    int isoX = bigx - bigy;
-   int isoY = (bigx + bigy) / 2;
-   al_draw_bitmap(sprite,isoX,isoY,0);
-   al_draw_bitmap(grid,isoX,isoY,0);
+
+   al_draw_bitmap(sprite,iso_x,iso_y,0);
+   al_draw_bitmap(grid,iso_x,iso_y,0);
 
 
 
