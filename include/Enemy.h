b0a924e3accdfcd03da3b56b7d595443d7a9b0fc
danwardvs <danwardvs@adsgames.net>
2018-05-05 03:33:04 +0000

    Enemy upd8

--- include/Enemy.h
+++ include/Enemy.h
@@ -3,6 +3,7 @@
 
 #include "allegro5/allegro.h"
 #include "tools.h"
+#include "allegro5/allegro_ttf.h"
 
 class Enemy
 {
@@ -21,6 +22,7 @@ class Enemy
   private:
     ALLEGRO_BITMAP *sprite;
     ALLEGRO_BITMAP *spritesheet[62];
+    ALLEGRO_FONT *font;
     int x=1000;
     int y=200;
     int frame=0;
