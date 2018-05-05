7a39d4d9cd3e02d7be18da8b17f563429434ea22
Our git is going to be a mess <ss.stobo@gmail.com>
2018-05-05 14:47:24 -0400

    LOOK I DID A CODE part 5

--- src/Cart.cpp
+++ src/Cart.cpp
@@ -25,11 +25,12 @@ Cart::~Cart()
 void Cart::update()
 {
   accel += 0.02f;
-  spin += 0.1f;
-    x_velocity += accel * -4;
-    y_velocity += accel * -4;
-    x += x_velocity;
-    y += y_velocity;
+  if(accel > 0.4f)
+    spin += 0.1f;
+  x_velocity += accel * -4;
+  y_velocity += accel * -4;
+  x += x_velocity;
+  y += y_velocity;
 
 }
 
